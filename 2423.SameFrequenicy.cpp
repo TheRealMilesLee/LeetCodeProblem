1a109e805cd1b24a71e2efcf855d3bdece2f4aa3
TheRealMilesLee <grandtheftauto710@outlook.com>
2023-09-23 18:13:13 -0700

    Update the documentation

--- 2423.SameFrequenicy.cpp
+++ 2423.SameFrequenicy.cpp
@@ -17,30 +17,39 @@ public:
   bool equalFrequency(string word)
   {
     int charCount[26] = {0};
-    for (char& c : word)
+/* This code snippet is iterating over each character `c` in the string `word` and incrementing the
+count of that character in the `charCount` array. The expression `c - 'a'` is used to convert the
+character `c` to an index in the `charCount` array. Since the characters in `word` are assumed to be
+lowercase letters, subtracting `'a'` from `c` gives the index of the character in the range of 0 to
+25 (corresponding to the letters 'a' to 'z'). */
+    for (char& characterIterate : word)
     {
-      charCount[c - 'a']++;
+      charCount[characterIterate - 'a']++;
     }
-    for (int i = 0; i < 26; i++)
+    /* This code snippet is iterating over each index `index` in the `charCount` array. If the count of
+    the character at that index is 0, it continues to the next iteration. Otherwise, it decrements
+    the count of that character by 1 and creates an unordered set `frequency` to store the
+    frequencies of the remaining characters. */
+    for (size_t index = 0; index < 26; index++)
     {
-      if (charCount[i] == 0)
+      if (charCount[index] == 0)
       {
         continue;
       }
-      charCount[i]--;
+      charCount[index]--;
       unordered_set<int> frequency;
-      for (int f : charCount)
+      for (int frequent : charCount)
       {
-        if (f > 0)
+        if (frequent > 0)
         {
-          frequency.insert(f);
+          frequency.insert(frequent);
         }
       }
       if (frequency.size() == 1)
       {
         return true;
       }
-      charCount[i]++;
+      charCount[index]++;
     }
     return false;
   }
