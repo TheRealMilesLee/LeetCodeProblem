02189446bf562d893f975ea27209b165a3f677c3
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-07-25 16:42:59 +0800

    Update the problem

--- ContainsDuplicate.py
+++ ContainsDuplicate.py
@@ -4,26 +4,26 @@ Given an integer array nums, return true if any value appears more than once in 
 import collections
 
 
-def solution(nums: list[int]) -> bool:
-  myDict = collections.defaultdict()
-  for index in nums:
-    if index not in myDict:
-      myDict[index] = 1
-    else:
-      myDict[index] += 1
+class Solution:
 
-  for _, values in myDict.items():
-    if values > 1:
-      return True
-  return False
+  def hasDuplicate(self, nums: list[int]) -> bool:
+    appearTime = collections.defaultdict()
+    for index in nums:
+      if int(index) not in appearTime.keys():
+        appearTime[int(index)] = 1
+      else:
+        appearTime[int(index)] += 1
+        return True
+    return False
 
 
 if __name__ == "__main__":
+  TestCase = Solution()
   nums1 = [1, 2, 3, 3]
   nums2 = [1, 2, 3, 4]
 
-  results1 = solution(nums1)
+  results1 = TestCase.hasDuplicate(nums1)
   print(results1)
 
-  results2 = solution(nums2)
+  results2 = TestCase.hasDuplicate(nums2)
   print(results2)
