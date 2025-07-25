4f18b9f3196ff59478ef4d3b6ea21329dcd199a5
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-07-25 20:52:15 +0800

    Update the two sum and valid anagram

---
+++ TwoSum.py
@@ -0,0 +1,32 @@
+"""
+Given an array of integers nums and an integer target, return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+
+Return the answer with the smaller index first.
+"""
+
+
+class Solution:
+
+  def twoSum(self, nums: list[int], target: int) -> list[int]:
+    prevMap = {}
+
+    for index, number in enumerate(nums):
+      diff = target - number
+      if diff in prevMap:
+        return [prevMap[diff], index]
+      prevMap[number] = index
+
+
+if __name__ == "__main__":
+  TestCase = Solution()
+
+  nums1 = [3, 4, 5, 6]
+  nums2 = [4, 5, 6]
+
+  results1 = TestCase.twoSum(nums1, 10)
+  print(results1)
+
+  results2 = TestCase.twoSum(nums2, 10)
+  print(results2)
--- ValidAnagram.py
+++ ValidAnagram.py
@@ -6,33 +6,31 @@ An anagram is a string that contains the exact same characters as another string
 import collections
 
 
-def solution(string1: str, string2: str) -> bool:
-  myDict = collections.defaultdict()
-  for index in string1:
-    if index in myDict:
-      myDict[index.lower()] += 1
-    else:
-      myDict[index.lower()] = 1
+class Solution:
 
-  myDict2 = collections.defaultdict()
-  for index2 in string2:
-    if index2 in myDict2:
-      myDict2[index2.lower()] += 1
-    else:
-      myDict2[index2.lower()] = 1
+  def isAnagram(self, s: str, t: str) -> bool:
+    if len(s) != len(t):
+      return False
 
-  if myDict2 == myDict:
+    count = [0] * 26
+    for i in range(len(s)):
+      count[ord(s[i]) - ord('a')] += 1
+      count[ord(t[i]) - ord('a')] -= 1
+
+    for val in count:
+      if val != 0:
+        return False
     return True
-  return False
 
 
 if __name__ == "__main__":
+  TestCase = Solution()
   string1 = "racecar"
   string2 = "carrace"
-  results1 = solution(string1, string2)
+  results1 = TestCase.isAnagram(string1, string2)
   print(results1)
 
   string3 = "jar"
   string4 = "jam"
-  results2 = solution(string3, string4)
+  results2 = TestCase.isAnagram(string3, string4)
   print(results2)
