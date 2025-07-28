05e4adfc2ce403c46db481077fe0c0854ff155b2
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-07-28 10:55:31 +0800

    Update a faster solution

--- ProductOfArray.py
+++ ProductOfArray.py
@@ -20,22 +20,44 @@ class Solution:
       resultArray.append(product)
     return resultArray
 
+  def productExceptFaster(self, nums: list[int]) -> list[int]:
+    length = len(nums)
+    result = [1] * length
+
+    # 左乘积
+    left_product = 1
+    for i in range(length):
+      result[i] = left_product
+      left_product *= nums[i]
+
+    # 右乘积
+    right_product = 1
+    for i in range(length - 1, -1, -1):
+      result[i] *= right_product
+      right_product *= nums[i]
+
+    return result
+
 
 if __name__ == "__main__":
   TestCase = Solution()
 
   Array1 = [1, 2, 4, 6]
   Result1 = TestCase.productExceptItSelf(Array1)
-  print(Result1)
+  Better1 = TestCase.productExceptFaster(Array1)
+  print(f"{Result1} {Better1} \n")
 
   Array2 = [-1, 0, 1, 2, 3]
   Result2 = TestCase.productExceptItSelf(Array2)
-  print(Result2)
+  Better2 = TestCase.productExceptFaster(Array2)
+  print(f"{Result2} {Better2} \n")
 
   Array3 = [1, 3, 5, 7]
   Result3 = TestCase.productExceptItSelf(Array3)
-  print(Result3)
+  Better3 = TestCase.productExceptFaster(Array3)
+  print(f"{Result3} {Better3} \n")
 
   Array4 = [9, -9, 0, 1]
   Result4 = TestCase.productExceptItSelf(Array4)
-  print(Result4)
+  Better4 = TestCase.productExceptFaster(Array4)
+  print(f"{Result4} {Better4} \n")
