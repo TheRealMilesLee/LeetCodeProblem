2973b0a288663f62d772ef7d5ff9c32faa6d0097
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-07-30 08:41:59 +0800

    update Valid palindrome

--- Chapter2-TwoPointer/1-ValiPalindrome.py
+++ Chapter2-TwoPointer/1-ValiPalindrome.py
@@ -8,18 +8,33 @@ A palindrome is a string that reads the same forward and backward. It is also ca
 class Solution:
 
   def isPalindrome(self, s: str) -> bool:
-    l, r = 0, len(s) - 1
+    toloweredString = s.lower()
+    filteredString = ""
+    for index in toloweredString:
+      if index.isalnum():
+        filteredString += index
 
-    while l < r:
-      while l < r and not self.alphaNum(s[l]):
-        l += 1
-      while r > l and not self.alphaNum(s[r]):
-        r -= 1
-      if s[l].lower() != s[r].lower():
+    ptrStart = 0
+    ptrEnd = len(filteredString) - 1
+    while ptrStart < len(filteredString) and ptrEnd > 0:
+      if filteredString[ptrStart] != filteredString[ptrEnd]:
         return False
-      l, r = l + 1, r - 1
+      ptrStart += 1
+      ptrEnd -= 1
     return True
 
-  def alphaNum(self, c):
-    return (ord('A') <= ord(c) <= ord('Z') or ord('a') <= ord(c) <= ord('z')
-            or ord('0') <= ord(c) <= ord('9'))
+
+if __name__ == "__main__":
+  TestCase = Solution()
+
+  string1 = "Was it a car or a cat I saw?"
+  Result1 = TestCase.isPalindrome(string1)
+  print(Result1)
+
+  string2 = "tab a cat"
+  Result2 = TestCase.isPalindrome(string2)
+  print(Result2)
+
+"""
+经典双指针, 需要注意的点就是要统一大小写, 并且要筛掉非数字和字母的字符. 然后剩下的就是简单的一头一尾两个指针向中间走就好了
+"""
