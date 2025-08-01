4d7648739e1fb67049b5b83df6dfe9bdaf55a051
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-08-01 21:03:52 +0800

    update the parenthesos

--- Chapter3-Stack/3-GenerateParentheses.py
+++ Chapter3-Stack/3-GenerateParentheses.py
@@ -6,26 +6,24 @@ Given an integer n. Return all well-formed parentheses strings that you can gene
 class Solution:
 
   def generateParenthesis(self, n: int) -> list[str]:
+    stack = []
     res = []
 
-    def valid(s: str):
-      open = 0
-      for c in s:
-        open += 1 if c == '(' else -1
-        if open < 0:
-          return False
-      return not open
-
-    def dfs(s: str):
-      if n * 2 == len(s):
-        if valid(s):
-          res.append(s)
+    def backtrack(openN, closedN):
+      if openN == closedN == n:
+        res.append("".join(stack))
         return
 
-      dfs(s + '(')
-      dfs(s + ')')
+      if openN < n:
+        stack.append("(")
+        backtrack(openN + 1, closedN)
+        stack.pop()
+      if closedN < openN:
+        stack.append(")")
+        backtrack(openN, closedN + 1)
+        stack.pop()
 
-    dfs("")
+    backtrack(0, 0)
     return res
 
 
