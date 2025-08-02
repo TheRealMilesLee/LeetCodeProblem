c8b376b9b9c1f8b65fd028fed74d0dc0ff3ce962
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-08-02 15:36:56 +0800

    Update the daily temperatures

--- Chapter3-Stack/4-DailyTemperatures.py
+++ Chapter3-Stack/4-DailyTemperatures.py
@@ -7,3 +7,25 @@ before a warmer temperature appears on a future day. If there is no day in the
 future where a warmer temperature will appear for the ith day, set result[i]
 to 0 instead
 """
+
+
+class Solution:
+
+  def dailyTemperatures(self, temperatures: list[int]) -> list[int]:
+    res = [0] * len(temperatures)
+    stack = []  # pair: [temp, index]
+
+    for i, t in enumerate(temperatures):
+      while stack and t > stack[-1][0]:
+        stackT, stackInd = stack.pop()
+        res[stackInd] = i - stackInd
+      stack.append((t, i))
+    return res
+
+
+if __name__ == "__main__":
+  TestCase = Solution()
+
+  temperatures = [30, 38, 30, 36, 35, 40, 28]
+  results = TestCase.dailyTemperatures(temperatures)
+  print(results)
