cf7bcd736592f96673a4cfd02f7c3e55ec5c7b28
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-08-04 11:56:19 +0800

    update the solution and comment

--- Chapter3-Stack/3-EvaluateReversePolishNotation.py
+++ Chapter3-Stack/3-EvaluateReversePolishNotation.py
@@ -15,23 +15,38 @@ class Solution:
   def evalRPN(self, tokens: list[str]) -> int:
     stack = []
 
+    operator = ["*", "+", "-", "/"]
     for index in tokens:
-      if index == "+":
-        a = stack.pop()
-        b = stack.pop()
-        stack.append(a + b)
-      elif index == "-":
-        a = stack.pop()
-        b = stack.pop()
-        stack.append(b - a)
-      elif index == "*":
-        a = stack.pop()
-        b = stack.pop()
-        stack.append(a * b)
-      elif index == "/":
-        a = stack.pop()
-        b = stack.pop()
-        stack.append(int(b / a))
-      else:
+      if index not in operator:
         stack.append(int(index))
+      else:
+        if index == "+":
+          a, b = stack.pop(), stack.pop()
+          stack.append(a + b)
+        elif index == "-":
+          a, b = stack.pop(), stack.pop()
+          stack.append(b - a)
+        elif index == "*":
+          a, b = stack.pop(), stack.pop()
+          stack.append(a * b)
+        else:
+          a, b = stack.pop(), stack.pop()
+          stack.append(int(b / a))
+
     return stack[0]
+
+
+if __name__ == '__main__':
+  TestCase = Solution()
+  operation = ["2", "1", "+", "3", "*"]
+  Result1 = TestCase.evalRPN(operation)
+  print(Result1)
+
+
+"""
+三步走思路
+
+1. 用array模拟栈, 走一个循环将数字转int以后存进去
+2. 如果遇到符号就pop两个出来进行运算, 注意运算顺序, 因为栈是lifo的所以对于除法和减法要反过来
+3. 将运算结果放回去
+"""
