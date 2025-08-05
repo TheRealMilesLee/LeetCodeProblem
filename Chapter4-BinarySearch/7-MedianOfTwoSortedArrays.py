2651509dbb37fd9021014e3cf8043dc7567e77e8
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-08-05 18:53:37 +0800

    Update the writing method

--- Chapter4-BinarySearch/7-MedianOfTwoSortedArrays.py
+++ Chapter4-BinarySearch/7-MedianOfTwoSortedArrays.py
@@ -21,9 +21,20 @@ class Solution:
       i = (left + right) // 2
       j = half - i - 2
 
-      Aleft = A[i] if i >= 0 else float("-infinity")
-      Aright = A[i + 1] if (i + 1) < len(A) else float("infinity")
-      Bleft = B[j] if j >= 0 else float("-infinity")
+      if i >= 0:
+        Aleft = A[i]
+      else:
+        Aleft = float("infinity")
+
+      if (i + 1) < len(A):
+        Aright = A[i + 1]
+      else:
+        Aright = float("infinity")
+
+      if j >= 0:
+        Bleft = B[j]
+      else:
+        Bleft = float("-infinity")
       Bright = B[j + 1] if (j + 1) < len(B) else float("infinity")
 
       if Aleft <= Bright and Bleft <= Aright:
