eaf9988d33c16265a4d00019db2faac730a5b06b
TheRealMilesLee <grandtheftauto710@outlook.com>
2025-08-09 13:50:22 +0800

    update the approach

--- Chapter5-SlidingWindow/6-SlidingWindowMaximum.py
+++ Chapter5-SlidingWindow/6-SlidingWindowMaximum.py
@@ -3,31 +3,31 @@ You are given an array of integers nums and an integer k. There is a sliding win
 
 Return a list that contains the maximum element in the window at each step.
 """
-
 from collections import deque
 
 
 class Solution:
 
   def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
-    output = []
-    q = deque()  # index
-    l = r = 0
+    q = deque()  # 存索引, 保证nums[q]递减
+    res = []
 
-    while r < len(nums):
-      while q and nums[q[-1]] < nums[r]:
+    for i, num in enumerate(nums):
+      # 弹出所有比当前num小的元素索引, 保证q单调递减
+      while q and nums[q[-1]] < num:
         q.pop()
-      q.append(r)
 
-      if l > q[0]:
+      q.append(i)
+
+      # 窗口左边界已经过期的索引弹出
+      if q[0] <= i - k:
         q.popleft()
 
-      if (r + 1) >= k:
-        output.append(nums[q[0]])
-        l += 1
-      r += 1
+      # 从窗口形成开始，收集结果
+      if i >= k - 1:
+        res.append(nums[q[0]])
 
-    return output
+    return res
 
 
 if __name__ == '__main__':
